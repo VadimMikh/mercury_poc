1fa9bb8d53363cf8326fa93a990ba64e09b059de
mykhailovv <mykhailovv@b2b.regn.net>
2020-06-11 13:19:28 +0200

    graphql request button improvment

--- src/pages/GrapnQL.js
+++ src/pages/GrapnQL.js
@@ -5,19 +5,23 @@ import { USERS } from '../graphql/requests';
 
 const GrapnQL = () => {
 	// const { loading, error, data } = useQuery(USERS);
-	const [ getData, { loading, data } ] = useLazyQuery(USERS);
-	const fetched = data?.users.length
+	const [ getData, { loading, data } ] = useLazyQuery(USERS)
+	const buttonData = {
+		class: data ? 'success' : 'warning',
+		text: data ? 'Done' : 'Make GraphQL request',
+		disabled: !!data
+	}
 	console.log('graphql response:', data)
 
 	return (
 		<Fragment>
 			<div className="text-center">
-				<button 
-					className="btn btn-warning mb-4" 
-					disabled={fetched}
-					onClick={() => getData()}>
-					Make GraphQL request
-				</button>
+			<button 
+				className={`btn btn-${buttonData.class} mb-4`} 
+				disabled={buttonData.disabled || loading}
+				onClick={() => getData()}>
+				{ buttonData.text }
+			</button>
 			</div>
 			{ data && (
 				<div className="row justify-content-center">
